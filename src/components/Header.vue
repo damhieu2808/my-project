382deeefc77ad84ef885258a7267cbc4a7561771
damhieu2808 <damhieu2808@gmail.com>
2022-03-28 21:22:18 +0700

    push 1

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container> 
+      <b-row class="header">
+          <b-col class="col-3">
+              <h1>HOA123</h1>
+          </b-col>
+          <b-col class="col-9">
+              <div>
+                  <div>
+                      <i class="fa-solid fa-phone"></i>
+                      <p>123</p>
+                  </div>
+              </div>
+          </b-col>
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header{
+    display: flex;
+}
+</style>
