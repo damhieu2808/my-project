aa3a915d9f2fe082e660f76d99acabcca9536aec
damhieu2808 <damhieu2808@gmail.com>
2022-03-28 21:32:12 +0700

    push 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,20 @@
 <template>
-  <b-container> 
-      <b-row class="header">
-          <b-col class="col-3">
-              <h1>HOA123</h1>
-          </b-col>
-          <b-col class="col-9">
-              <div>
-                  <div>
-                      <i class="fa-solid fa-phone"></i>
-                      <p>123</p>
-                  </div>
-              </div>
-          </b-col>
-      </b-row>
+  <b-container>
+    <b-row class="header">
+      <b-col class="col-3">
+        <p>HOA123</p>
+      </b-col>
+      <b-col class="col-9">
+        <div>
+          <div class="phone">
+            <span>
+              <i class="fa-solid fa-phone"></i>
+            </span>
+            <span class="mb-0">1900 9217</span>
+          </div>
+        </div>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -21,7 +23,13 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.header{
+.header {
+  display: flex;
+  align-items: center;
+}
+.col-9 {
+  .phone {
     display: flex;
+  }
 }
 </style>
