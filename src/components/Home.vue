a12cfb248cbf124e724621faa99643f387d43b4b
Xuan Hoa <75961133+eriklehnsher@users.noreply.github.com>
2022-04-10 16:42:32 +0700

    123

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,89 +1,103 @@
 <template>
-  <div class="home">
-    <div class="banner">
-      <div class="search-box">
-        <p class="title sub-header">HaNoiCAR - CÙNG BẠN TRÊN MỌI HÀNH TRÌNH</p>
-        <div class="container tab-search">
-          <b-card no-body>
-            <b-tabs pills card vertical>
-              <b-tab active>
-                <template #title>
-                  <img src="../assets/xe_tu_lai.png" />
-                  <p class="text-tab">Xe tự lái</p>
-                </template>
-                <div>
-                  <b-form-group label="Địa điểm" label-for="input-1">
-                    <div class="location">
-                      <i class="fa-solid fa-location-dot"></i>
-                      <b-form-input
-                        id="input-1"
-                        v-model="text"
-                        placeholder="Nhập thành phố, quận, địa chỉ..."
-                        style="outline: none"
-                      ></b-form-input>
-                    </div>
-                  </b-form-group>
-                   <div class="form-search has-timer">
-                      <label class="home-label">Bắt Đầu</label>
-                      <div class="d-flex">
-                        <div class="wrap-input home-datetimerange">
-                          <div class="react-bootstrap-daterangepicker-container" style="display: inline-block;">
-                            <span class="value">
-                              "06/04/2022"
-                              <i class="i-chevron-down" style="position: static;"></i>
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-              </b-tab>
-              <b-tab title="Xe lái tự dộng">
-                <template #title>
-                  <img back class="photo-tab2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Man_Driving_Car_Cartoon_Vector.svg/2560px-Man_Driving_Car_Cartoon_Vector.svg.png" />
-                  <p class="text-tab">Xe lai tu dong</p>
-                </template>
-                <div class="search search-tab2">
-
-                </div>
-              </b-tab>
-            </b-tabs>
-          </b-card>
-        </div>
-      </div>
+  <div class="container body">
+    <div class="search-box">
+      <p class="slogan">HNCAR cung ban tren moi neo duong</p>
     </div>
+    <div>
+  <b-card no-body>
+    <b-tabs card>
+      <b-tab title="Xe tự Lái" active>
+        <b-card-text>Tab contents 1</b-card-text>
+        <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="địa điểm:"
+        label-for="input-1"
+       
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type=""
+          placeholder="Nhập TP, quận, địa chỉ..."
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Bắt đầu" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+
+        <b-form-group id="input-group-2" label="kết thúc" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder=""
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Tìm xe ngay</b-button>
+      
+    </b-form>
+
+  </div>
+      </b-tab>
+      <b-tab title="Xe có tài xế">
+        <b-card-text>Tab contents 2</b-card-text>
+      </b-tab>
+    </b-tabs>
+  </b-card>
+</div>
   </div>
 </template>
 
 <script>
-export default {};
+ export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.food = null
+        this.form.checked = []
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="scss">
-.tab-search{
-  padding: 5px;
- background-color: gray;
+.search-box {
+ padding: 5px;
+ margin: 5px;
 }
-.location {
-  display: flex;
-  align-items: center;
-}
-.sub-header{
-  display: flex;
-  font-size: 40px;
-  justify-content: center;
-  line-height: 50px;
-  font-weight: 700;
-  text-align: center;
-  margin-bottom: 0.25rem;
-  margin-top: 6rem;
-}
-.fa-location-dot{
-  margin: 5px;
-  color: green;
-}
-.photo-tab2 {
-  width: 200px;
-  height: 200px;
-}
+
 </style>
